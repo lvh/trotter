4020901f9bd5bfa1660f59dae00d771dfa16f48e
Laurens Van Houtven <_@lvh.io>
2014-08-14 12:10:31 +0200

    Add some compojure and ring for good measure

--- project.clj
+++ project.clj
@@ -6,5 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.palletops/pallet "0.8.0-RC.9"]
                  [com.palletops/pallet-jclouds "1.7.3"]
-                 [org.apache.jclouds.provider/rackspace-cloudservers-us "1.8.0"]]
+                 [org.apache.jclouds.provider/rackspace-cloudservers-us "1.8.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-core "1.3.0"]
+                 [ring/ring-jetty-adapter "1.3.0"]
+                 [ring/ring-json "0.3.1"]]
   :profiles {:dev {:plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}})
